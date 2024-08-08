7db2abea07779d78372fc803743936e8818992dd
Lxdovic <ludovicdebever0@gmail.com>
2024-08-08 20:07:54 +0200

    1.6 - File Copying

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-#define LOWER 0
-#define UPPER 300
-#define STEP 20
-
 int main(void) {
-    for (int fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
-        printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+    int c;
+    while ((c = getchar()) != EOF) {
+        if ((c != EOF) == 0 || (c != EOF) == 1) {
+            putchar(c);
+        }
+    }
 }
 
