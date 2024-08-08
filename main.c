99cdcaaff76825e923e7e3e50e9c934d63cf1ae6
Lxdovic <ludovicdebever0@gmail.com>
2024-08-08 19:42:35 +0200

    1.2 - Fahrenheit & Celsius Table 2

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-    int fahr, celsius;
-    int lower, upper, step;
+    float fahr, celsius;
+    float lower, upper, step;
 
     lower = 0;
     upper = 300;
@@ -11,9 +11,9 @@ int main(void) {
     fahr = lower;
 
     while (fahr <= upper) {
-        celsius = 5 * (fahr - 32) / 9;
+        celsius = (5.0/9.0) * (fahr - 32.0);
 
-        printf("%3d\t%6d\n", fahr, celsius);
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr += step;
     }
 }
