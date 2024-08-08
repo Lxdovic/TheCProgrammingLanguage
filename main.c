cce64eecfcb21c5aa917756bbccc8cdfa1dd6393
Lxdovic <ludovicdebever0@gmail.com>
2024-08-08 20:08:37 +0200

    1.7 - File Copying

--- main.c
+++ main.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 
 int main(void) {
-    int c;
-    while ((c = getchar()) != EOF) {
-        if ((c != EOF) == 0 || (c != EOF) == 1) {
-            putchar(c);
-        }
-    }
+    printf("%d", EOF);
 }
 
