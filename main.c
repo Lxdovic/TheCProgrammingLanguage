54559413fb47492734e3147889522bfe72c5a68a
Lxdovic <ludovicdebever0@gmail.com>
2024-08-09 08:24:08 +0200

    1.8 - Counting Blanks, Tabs and NewLines

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("%d", EOF);
+    int b = 0;
+    int tab = 0;
+    int nl = 0;
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        switch (c) {
+            case '\t':
+                ++tab;
+                break;
+            case ' ':
+                ++b;
+                break;
+            case '\n':
+                ++nl;
+                break;
+            default:
+                break;
+        }
+    }
+
+    printf("Blanks: %d\nTabs: %d\nNewlines: %d\n", b, tab, nl);
 }
 
