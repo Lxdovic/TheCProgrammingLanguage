71c3e3575195e1d0e8db41a6b9e3f6570949914b
Lxdovic <ludovicdebever0@gmail.com>
2024-08-10 19:55:36 +0200

    1.13 - Print Histogram of words lengths

--- main.c
+++ main.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 
 int main(void) {
-    int c;
+    int c, l;
+    int lengths[10];
 
     while ((c = getchar()) != EOF) {
         if (c == ' ' || c == '\t' || c == '\n') {
-            putchar('\n');
-        } else {
-            putchar(c);
+            if (l > 0 && l < 10) {
+                lengths[l]++;
+            }
+
+            l = 0;
+            continue;
         }
+
+        l++;
+    }
+
+    if (l > 0 && l < 10) {
+        lengths[l]++;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d: ", i);
+        for (int j = 0; j < lengths[i]; j++) {
+            printf("*");
+        }
+        printf("\n");
     }
 
     return 0;
+
 }
 
