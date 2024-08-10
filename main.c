68e5cdcc2adc2bc0fd70a35da1a57681e5658889
Lxdovic <ludovicdebever0@gmail.com>
2024-08-10 16:48:41 +0200

    1.12 - Write input one word/line

--- main.c
+++ main.c
@@ -4,22 +4,10 @@ int main(void) {
     int c;
 
     while ((c = getchar()) != EOF) {
-        switch (c) {
-            case '\\':
-                putchar('\\');
-                putchar('\\');
-                break;
-            case '\t':
-                putchar('\\');
-                putchar('t');
-                break;
-            case '\b':
-                putchar('\\');
-                putchar('b');
-                break;
-            default:
-                putchar(c);
-                break;
+        if (c == ' ' || c == '\t' || c == '\n') {
+            putchar('\n');
+        } else {
+            putchar(c);
         }
     }
 
