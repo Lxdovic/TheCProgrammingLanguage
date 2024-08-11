9449d841b9434777a3ae2e9bdfe1bf8521253e16
Lxdovic <ludovicdebever0@gmail.com>
2024-08-11 10:22:56 +0200

    1.16 - Longest line -> arbitrary length

--- main.c
+++ main.c
@@ -1,24 +1,54 @@
 #include <stdio.h>
 
-#define LOWER 0
-#define UPPER 300
-#define STEP 20
+#define MAXLINE 1000
 
-float fahrToCelsius(float fahr);
+/* maximum input line length */
+int _getline(char s[], int maxline);
+
+void copy(char to[], char from[]);
 
 int main(void) {
-    float fahr, celsius;
+    int len;
+    int max;
+    int c;
+    char line[MAXLINE];
+    char longest[MAXLINE];
 
-    fahr = LOWER;
+    max = 0;
 
-    while (fahr <= UPPER) {
-        celsius = fahrToCelsius(fahr);
-        printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + STEP;
+    while ((len = _getline(line, MAXLINE)) > 0) {
+        if (line[len - 1] != '\n') {
+            while ((c = getchar()) != EOF && c != '\n') {
+                ++len;
+            }
+        }
+
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+        }
     }
+
+    if (max > 0)
+        printf("%i %s", max, longest);
+    return 0;
 }
 
-float fahrToCelsius(float fahr) {
-    return (5.0 / 9.0) * (fahr - 32.0);
+int _getline(char s[], int lim) {
+    int c, i;
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
 }
 
+void copy(char to[], char from[]) {
+    int i;
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}
