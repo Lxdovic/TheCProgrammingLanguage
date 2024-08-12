195535e4242c9a9d353d93db67020098e7b0a9ad
Lxdovic <ludovicdebever0@gmail.com>
2024-08-12 10:04:18 +0200

    1.19 - Find the ranges of primitive number types programmatically > bug

--- main.c
+++ main.c
@@ -1,45 +1,94 @@
 #include <stdio.h>
 
-#define MAXLINE 1000
+void print_char_range();
 
-void reverse(char input[]);
+void print_short_range();
 
-int get_line(char s[], int limit);
+void print_int_range();
 
 int main(void) {
-    char line[MAXLINE];
+    // for some reason I cant explain for now, separating these in different functions
+    // seems to make them break.
 
-    while ((get_line(line, MAXLINE)) > 0) {
-        reverse(line);
-        printf("%s", line);
+    // print_char_range();
+    unsigned char ucharMin;
+    unsigned char ucharMax;
+
+    for (ucharMin = 0; ucharMin >= 0; ucharMin++) {
+        if (ucharMax - ucharMin > 1) break;
+
+        ucharMax = ucharMin;
     }
+
+    printf("unsigned char -> min: %i, max: %i\n", ucharMin, ucharMax);
+
+    char charMin;
+    char charMax;
+
+    for (charMin = 0; charMin >= 0; charMin++) {
+        if (charMax - charMin > 1) break;
+
+        charMax = charMin;
+    }
+
+    printf("signed char -> min: %i, max: %i\n", charMin, charMax);
+
+    // print_short_range();
+    unsigned short ushortMin;
+    unsigned short ushortMax;
+
+    for (ushortMin = 0; ushortMin >= 0; ushortMin++) {
+        if (ushortMax - ushortMin > 1) break;
+
+        ushortMax = ushortMin;
+    }
+
+    printf("unsigned short -> min: %i, max: %i\n", ushortMin, ushortMax);
+
+    short shortMin;
+    short shortMax;
+
+    for (shortMin = 0; shortMin >= 0; shortMin++) {
+        if (shortMax - shortMin > 1) break;
+
+        shortMax = shortMin;
+    }
+
+    printf("signed short -> min: %i, max: %i\n", shortMin, shortMax);
+
+    // print_int_range();
+    unsigned int uintMin;
+    unsigned int uintMax;
+
+    for (uintMin = 0; uintMin >= 0; uintMin++) {
+        if (uintMax - uintMin > 1) break;
+
+        uintMax = uintMin;
+    }
+
+    printf("unsigned int -> min: %i, max: %i\n", uintMin, uintMax);
+
+    int intMin;
+    int intMax;
+
+    for (intMin = 0; intMin >= 0; intMin++) {
+        if (intMax - intMin > 1) break;
+
+        intMax = intMin;
+    }
+
+    printf("signed int -> min: %i, max: %i\n", intMin, intMax);
+
+    return 0;
 }
 
-void reverse(char input[]) {
-    int length = 0;
-    int i;
-
-    for (i = 0; input[i] != '\n'; i++)
-        length++;
-
-    for (int i = 0; i < length / 2; i++) {
-        input[i] ^= input[length - 1 - i];
-        input[length - 1 - i] ^= input[i];
-        input[i] ^= input[length - 1 - i];
-    }
+void print_char_range() {
 }
 
-int get_line(char s[], int limit) {
-    int c = 0;
-    int i;
-
-    for (i = 0; i < limit - 1 && (c = getchar()) != '\n' && c != EOF; ++i) {
-        s[i] = c;
-    }
-    if (c == '\n') {
-        s[i] = c;
-        ++i;
-    }
-    s[i] = '\0';
-    return i;
+void print_short_range() {
 }
+
+void print_int_range() {
+}
+
+
